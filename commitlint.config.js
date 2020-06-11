418d932e01c4dacbabe05f2396015e20c72f2637
Marco Pasqualetti <marco.pasqualetti@live.com>
2020-06-11 23:56:24 +0200

    chore(lint): refine commit rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']]
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']],
+    'scope-enum': [2, 'always', ['db', 'server', 'editor', 'template', 'lint']]
   }
 };
