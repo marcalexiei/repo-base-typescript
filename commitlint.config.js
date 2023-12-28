fdd1e5ae1deb60c98fb85ff2fde891cfd32c381a
Marco Pasqualetti <marco.pasqualetti@live.com>
2023-12-28 12:45:35 +0100

    chore(deps-dev): update commitlint

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test']],
-    'scope-enum': [2, 'always', ['db', 'server', 'editor', 'template', 'lint']]
-  }
-};
